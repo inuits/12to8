d30b23270758528d2fce5e35684e7f70486438d7
Julien Pivotto <roidelapluie@inuits.eu>
2017-09-01 20:47:31 +0200

    Use viper for endpoint config
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/inuits/12to8/api"
@@ -74,15 +75,18 @@ func initConfig() {
 
 	viper.SetConfigName(".12to8")          // name of config file (without extension)
 	viper.AddConfigPath(os.Getenv("HOME")) // adding home directory as first search path
-	viper.AutomaticEnv()                   // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	//	viper.SetEnvPrefix("twelve_to_eight")  // env variables can't start with a number
+	viper.AutomaticEnv() // read in environment variables that match
+	viper.ReadInConfig()
 }
 
 func NewApiClient() api.Client {
+	username := viper.GetString("username")
+	password := viper.GetString("password")
+	endpoint := viper.GetString("endpoint")
+	if endpoint == "" {
+		log.Fatal("Endpoint is not set!")
+	}
 	return api.Client{
 		Username: username,
 		Password: password,
